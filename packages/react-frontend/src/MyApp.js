9faaaf60e33e1a6e8d1c87b14350021e7ef0a3e1
misakitanabe <misaki.a.tanabe@gmail.com>
2024-01-11 12:42:50 -0800

    Added Form.js to dynamically add characters

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,26 +1,27 @@
 // src/MyApp.js
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 function MyApp() {
     const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
+        // {
+        //     name: "Charlie",
+        //     job: "Janitor"
+        // },
+        // {
+        //     name: "Mac",
+        //     job: "Bouncer"
+        // },
+        // {
+        //     name: "Dee",
+        //     job: "Aspring actress"
+        // },
+        // {
+        //     name: "Dennis",
+        //     job: "Bartender"
+        // }
     ])
 
     function removeOneCharacter(index) {
@@ -36,6 +37,7 @@ function MyApp() {
                 characterData={characters}
                 removeCharacter = {removeOneCharacter}
             />
+            <Form />
         </div>
     );
 }
